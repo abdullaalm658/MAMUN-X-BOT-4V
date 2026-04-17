76447efcb3c88ccdb2bfbf2ae314974e326b66ad
MAMUN X BOT V4 <mamunranam896@gmail.com>
2026-04-18 03:11:35 +0600

    Update updateZipUrl to new repository link

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ const settings = {
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.7",
-  updateZipUrl: "",
+  updateZipUrl: "https://github.com/abdullaalm658/MAMUN-X-BOT-4V.git",
 };
 
 module.exports = settings;
