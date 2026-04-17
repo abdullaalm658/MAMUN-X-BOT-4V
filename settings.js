b7fc63e7bfcc11b1f5af69bedace12bf766c3e3e
abdullaalm658 <mamunranam896@gmail.com>
2026-04-18 01:48:07 +0600

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'MAMUN X BOTt',
+  author: '‎',
+  botName: "MAMUN X BOT",
+  botOwner: 'Professor', // Your name
+  ownerNumber: '8801786269298', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "",
+};
+
+module.exports = settings;
